4aa511c2fdde6b1c9d2de909650b04985544bbae
Daniel151505 <daniel150520011@gmail.com>
2020-10-19 02:06:41 -0500

    Calcular Metodos

--- Calculadoramvc/Controllers/CalculadoraController.cs
+++ Calculadoramvc/Controllers/CalculadoraController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Calculadoramvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVC_Lab.Models;
@@ -28,10 +29,16 @@ namespace MVC_Lab.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+
+        public IActionResult Calcular(Calculadora objCalculadora){
+    
+         if("+".Equals(objCalculadora.Accion)){
+            objCalculadora.Respuesta = objCalculadora.Operador1 = objCalculadora.Operador2;
+         }
+         return View(objCalculadora);
+
         }
+
     }
 }
